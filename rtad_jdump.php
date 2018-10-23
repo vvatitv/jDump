2db1726b7e76e015c51da3a64592f51a43d68f9b
Vladimir 'def' Titov <vvatitv@gmail.com>
2018-10-23 14:05:27 +0300

    start

---
+++ rtad_jdump.php
@@ -0,0 +1,24 @@
+<?php
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+// jimport( 'joomla.event.helper' );
+
+// if( file_exists( JPATH_SITE.'/plugins/system/laravel_dump/vendor/autoload.php' ) ) {
+//     require_once( JPATH_SITE.'/plugins/system/laravel_dump/vendor/autoload.php' );
+// } else {
+//     JError::raiseNotice( 20, 'The Plugin needs autoload.php for enable function.' );
+// }
+
+// if(!function_exists('is_json')){
+// 	function is_json($string){
+// 		return is_string($string) && is_array(json_decode($string, true)) && (json_last_error() == JSON_ERROR_NONE) ? true : false;
+//    	}
+// }
+
+class plgSystemRtadJdump extends JPlugin {
+    function __construct(& $subject, $params) {
+        parent::__construct($subject, $params);
+    }
+	function onAfterRender(){}
+}

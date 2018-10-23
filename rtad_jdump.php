762bbb41ee541361656af7a8115dabeaeb879c6d
Vladimir 'def' Titov <vvatitv@gmail.com>
2018-10-23 14:54:40 +0300

    update

--- rtad_jdump.php
+++ rtad_jdump.php
@@ -4,8 +4,8 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 jimport( 'joomla.event.helper' );
 
-if( file_exists( JPATH_SITE.'/plugins/system/rtad_jdump/vendor/autoload.php' ) ) {
-    require_once( JPATH_SITE.'/plugins/system/rtad_jdump/vendor/autoload.php' );
+if( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+    require_once( __DIR__ . '/vendor/autoload.php' );
 } else {
     JError::raiseNotice( 20, 'The Plugin needs autoload.php for enable function.' );
 }
@@ -16,9 +16,24 @@ if(!function_exists('is_json')){
    	}
 }
 
-class plgSystemRtadJdump extends JPlugin {
-    function __construct(& $subject, $params) {
-        parent::__construct($subject, $params);
+class plgSystemRtadJdump extends JPlugin{
+    public function __construct(& $subject, $params){
+		parent::__construct($subject, $params);
     }
-	function onAfterRender(){}
+	public function onAfterInitialise(){
+
+		if ( JFactory::getApplication()->isAdmin() ){
+			 return;
+		}
+		if ( $this->params->get('mootools', 0) == '1' ){
+			require_once (__DIR__ . '/lib/behavior.php');
+		}
+		if ( $this->params->get('jquery', 0) == '1' ){
+			require_once (__DIR__ . '/lib/jquery.php');
+		}
+		if ( $this->params->get('bootstrap', 0) == '1' ){
+			require_once (__DIR__ . '/lib/bootstrap.php');
+		}
+
+	}
 }
